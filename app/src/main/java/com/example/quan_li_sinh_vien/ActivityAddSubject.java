779f92c0ea44a691cb0a06b7f918c28545b6b496
iFrancez <phannguyenthanhphap@gmail.com>
2023-04-27 11:01:27 +0700

    edit listview && database

---
+++ app/src/main/java/com/example/quan_li_sinh_vien/ActivityAddSubject.java
@@ -0,0 +1,14 @@
+package com.example.quan_li_sinh_vien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityAddSubject extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_subject);
+    }
+}
